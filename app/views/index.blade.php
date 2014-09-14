2bb7b71832363b8fcb654f4cb1c990e49b18d58c
metalikium <Metalikium@users.noreply.github.com>
2014-09-14 15:31:37 +0200

    Off-canvas menu

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -7,10 +7,32 @@
 	<link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}" />
 	<link rel="stylesheet" href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css') }}" />
 	<link rel="stylesheet" href="{{ asset('bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
 </head>
 <body ng-app="movieApp" ng-controller="mainController">
-	
+	<nav id="myNavmenu" class="navmenu navmenu-default navmenu-fixed-left offcanvas" role="navigation">
+		<ul class="nav navmenu-nav">
+			<span class="navmenu-brand">Menu</span>
+			<li class ="active"><a href="#">Item 1</a></li>
+			<li><a href="#">Item 2</a></li>
+			<li><a href="#">Item 3</a></li>
+		</ul>
+	</nav>
+	<div class="navbar navbar-default navbar-fixed-top">
+		<a href="/" title="Watch it later" class="logotype">Watch it later</a>
+
+		<button type="button" class="navbar-toggle" style="display: block; float: left;" data-toggle="offcanvas" data-target="#myNavmenu" data-canvas="body">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+	</div>
+
 	<div class="container">
+		<div class="page-header">
+			<h1>Movies list</h1>
+		</div>
+
 		<p class="text-center" ng-show="loading"><span class="fa fa-refresh fa-spin fa-2x"></span></p>
 
 		<ul class="list" ng-hide="loading">
